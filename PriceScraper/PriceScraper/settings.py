20e5cb94c7f4a21f538526985ea3ab2bc29c833e
rezardes <rezardes@gmail.com>
2017-08-06 06:06:53 +0700

    Modify Settings
    Modify Settings

--- PriceScraper/PriceScraper/settings.py
+++ PriceScraper/PriceScraper/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'PriceScraper.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [
+        'DIRS': [ # <-- TEMPAT TEMPLATE -->
             os.path.join(BASE_DIR, 'search/templates'),
         ],
         'APP_DIRS': True,
@@ -120,3 +120,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+
+MEDIA_URL = '/media/'
